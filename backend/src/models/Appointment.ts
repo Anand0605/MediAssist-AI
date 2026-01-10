0268df032fe7d1b79cb863bbc6de1740c6f7c6f4
Anand Kumar Gautam <anandvns0510@gmail.com>
2026-01-10 23:55:38 +0530

    update

---
+++ backend/src/models/Appointment.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema({
+  patientId: mongoose.Schema.Types.ObjectId,
+  doctor: String,
+  date: String,
+  status: String,
+});
+
+export default mongoose.model("Appointment", appointmentSchema);
